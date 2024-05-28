1ff556bd2f41b0f6dc59dd22abfc7fa649defa15
Rio Vernando <riovernando70@gmail.com>
2024-05-28 11:37:55 +0700

    first commit

---
+++ DOM Manipulation/script.js
@@ -0,0 +1,11 @@
+// Manipulation Element
+
+// element.innerHTML = '...'
+const judul = document.getElementById('judul');
+judul.innerHTML = '<em>Kristian Rio Vernando</em>';
+
+// element.setAttribute()
+judul.setAttribute('name', 'rio');
+
+// element.getAttribute()
+judul.getAttribute('id'); // output : judul
